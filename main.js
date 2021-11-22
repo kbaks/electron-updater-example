1c24241dc6c7bf39a68e155d0d3c21e3491daf20
Konstantinos Bakas <konstantinos.bakas@arhs-dev-hellas.com>
2021-11-22 14:56:50 +0200

    moved to v0.8.8

--- main.js
+++ main.js
@@ -90,9 +90,9 @@ autoUpdater.on('download-progress', (progressObj) => {
   log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
   sendStatusToWindow(log_message);
 })
-autoUpdater.on('update-downloaded', (info) => {
-  sendStatusToWindow('Update downloaded');
-});
+// autoUpdater.on('update-downloaded', (info) => {
+//   sendStatusToWindow('Update downloaded');
+// });
 app.on('ready', function() {
   // Create the Menu
   const menu = Menu.buildFromTemplate(template);
